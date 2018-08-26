31ce23a8a063734d79de2096f38e7d2c61c0550d
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-25 20:23:19 -0600

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,7 +2,11 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
 _contents := $(_contents)    "Venom":{$(newline)
 
+# Target variables default values
+TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
+
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_str, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
