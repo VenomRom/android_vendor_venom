SuperiorOS/android_vendor_superior

vendor: Fruity pebbles [2/4] *Dark theme edits* - Add in styles to theme the package installer - Theme DND QS panel text - Theme Recents FAB - Theme conditions/suggestions background
Change-Id: Ie6e916691449af4b4c3d652d2be6fd9924ac3c8b

config/common.mk
@@ -173,6 +173,9 @@ $(call inherit-product, vendor/superior/config/telephony.mk)
 # Themes
 include vendor/themes/common.mk
 
+# Packages
+include vendor/superior/config/packages.mk
+
 # Superior_props
 $(call inherit-product, vendor/superior/config/superior_props.mk)
 
@@ -187,4 +190,3 @@ endif
 #        include vendor/superior/sdclang/sdclang.mk
 #    endif
 #endif
-

VenomRom/android_vendor_venom

config/common.mk
@@ -176,6 +176,9 @@ $(call inherit-product, vendor/venom/config/venom_props.mk)
 # Themes
 include vendor/themes/common.mk
 
+# Packages
+include vendor/venom/config/packages.mk
+
 # Enable ADB authentication
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
@@ -188,4 +191,3 @@ endif
 #        include vendor/venom/sdclang/sdclang.mk
 #    endif
 #endif
-