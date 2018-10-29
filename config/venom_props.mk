5522de9423c8f1fcb258d311dc1fffa0468e3fb1
Syed Sajid <syedsajid341@gmail.com>
2018-10-29 00:38:20 +0000

    fix

--- config/venom_props.mk
+++ config/venom_props.mk
@@ -42,7 +42,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Set cache location
 ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),) 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.device.cache_dir=/data/cache else PRODUCT_DEFAULT_PROPERTY_OVERRIDES 
-+= \
+    ro.device.cache_dir=/data/cache 
+else 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.device.cache_dir=/cache
 endif
